54298830bab6f6caf9e13a027ed5ee2efedc7e84
Miguel Fernandez Gargurevich <miguel.gargurevich@outlook.com>
2025-07-07 18:43:03 -0500

    chore: cleanup - elimina src y config de la raíz, solo frontend como fuente principal

--- frontend/dist/gargurevich-dev-website/server/main.server.mjs
+++ frontend/dist/gargurevich-dev-website/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PYDLBOXO.mjs";import"./chunk-V7DNT7SM.mjs";import"./chunk-CDBRTF5H.mjs";import"./chunk-3LBIKIXF.mjs";import"./chunk-MCBTUB5Z.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RBDZXVV6.mjs";import"./chunk-V7DNT7SM.mjs";import"./chunk-CDBRTF5H.mjs";import"./chunk-3LBIKIXF.mjs";import"./chunk-MCBTUB5Z.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
