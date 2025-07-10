207720257e8d01bc0406fe8c2e5ad1b5fbf66a03
Miguel Fernandez Gargurevich <miguel.gargurevich@outlook.com>
2025-07-10 18:41:24 -0500

    build: último build de frontend y ajustes manuales en lógica de tema PDF (oscuro por defecto, log de depuración)

--- frontend/dist/server/main.server.mjs
+++ frontend/dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GLGFSGUD.mjs";import"./chunk-RR654QU4.mjs";import"./chunk-SHFW5ZOV.mjs";import"./chunk-SJEA5TJ5.mjs";import"./chunk-CAOMNRW6.mjs";import"./chunk-X65Z4QIY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ZVV425JI.mjs";import"./chunk-RR654QU4.mjs";import"./chunk-SHFW5ZOV.mjs";import"./chunk-SJEA5TJ5.mjs";import"./chunk-CAOMNRW6.mjs";import"./chunk-X65Z4QIY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
